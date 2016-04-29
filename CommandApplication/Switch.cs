e440fe4327ad3e0adcf59cac82e87566de602557
Carsten Wollsen <carsten.wollsen@gmail.com>
2016-04-29 14:22:03 +0200

    switch commands only execute if different from the last one executed

--- CommandApplication/Switch.cs
+++ CommandApplication/Switch.cs
@@ -1,23 +1,39 @@
-﻿namespace CommandApplication
+﻿using System;
+
+namespace CommandApplication
 {
 	public class Switch
 	{
 		private readonly ICommand _closedCommand;
 		private readonly ICommand _openedCommand;
+		private ICommand lastExecuted;
 
 		public Switch(ICommand closedCommand, ICommand openedCommand)
 		{
 			_closedCommand = closedCommand;
 			_openedCommand = openedCommand;
+			lastExecuted = null;
 		}
 
 		public void Close()
 		{
+			if(lastExecuted == _closedCommand)
+			{
+				Console.WriteLine("Already closed");
+				return;
+			}
+			lastExecuted = _closedCommand;
 			_closedCommand.Execute();
 		}
 
 		public void Open()
 		{
+			if(lastExecuted == _openedCommand)
+			{
+				Console.WriteLine("Already open");
+				return;
+			}
+			lastExecuted = _openedCommand;
 			_openedCommand.Execute();
 		}
 
