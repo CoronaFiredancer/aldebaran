97fe4f427733d91020a36bd390109c5ba9567d59
Carsten Wollsen <carsten.wollsen@gmail.com>
2016-05-04 15:27:34 +0200

    cleaned up the client

--- CommandApplication/Client.cs
+++ CommandApplication/Client.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Autofac;
-using CommandApplication.Commands;
 using CommandApplication.Containers;
 using CommandApplication.Switchables;
 using CommandApplication.Switches;
@@ -19,32 +18,18 @@ namespace CommandApplication
 
 			using (var scope = Container.BeginLifetimeScope())
 			{
-				//var commands = scope.Resolve<IEnumerable<ICommand>>();
-				//var commandsList = commands.ToList();
-
 				var invoker = scope.Resolve<ISwitch>();
 				ClientExecutor.Execute(invoker);
 
 				var flowValve = scope.Resolve<IEnumerable<ISwitchable>>().ToList()[1];
-				
-
-				//var valveClose = commandsList[0];
-				//valveClose.Switchable = flowValve;
-				//var valveOpen = commandsList[1];
-				//valveOpen.Switchable = flowValve;
-
 				var ventil = scope.Resolve<ISwitch>();
+
 				foreach (var command in ventil.Commands)
 				{
 					command.Switchable = flowValve;
 				}
-				//ventil.ClearCommands();
-				//ventil.AddCommand(valveClose);
-				//ventil.AddCommand(valveOpen);
-				
 
 				ClientExecutor.Execute(ventil);
-
 			}
 		}
 	}
